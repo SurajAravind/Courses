33a5de1a02cfbe1339dcef6e3899b202026b1f5a
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2024-09-10 08:40:57 +0530

    Update 06_fibonocci.c
    Added the 3 types of looping constructs

--- 06_fibonocci.c
+++ 06_fibonocci.c
@@ -1,4 +1,5 @@
 // Program related to Fibonacci Series
+// Making use of the 3 types of loop constructs
 
 /* The Fibonacci sequence is a sequence where the next term is the sum of the previous two terms. The first two terms of the Fibonacci sequence are 0 followed by 1. */
 
@@ -19,30 +20,39 @@ int main() {
   printf("Fibonacci Series: \n%d \n%d", fibonacci_1, fibonacci_2);
 
   // print 3rd to nth terms
-  for (int i = 3; i <= number_of_terms; i++) {
+  /*for (int i = 3; i <= number_of_terms; i++) {
     printf("\n%d", fibonacci_3);
     fibonacci_1 = fibonacci_2;
     fibonacci_2 = fibonacci_3;
     fibonacci_3 = fibonacci_1 + fibonacci_2;
-  }
+  }*/
+  int i = 3;
+  /*while (i <= number_of_terms) {
+    printf("\n%d", fibonacci_3);
+    fibonacci_1 = fibonacci_2;
+    fibonacci_2 = fibonacci_3;
+    fibonacci_3 = fibonacci_1 + fibonacci_2;
+    i++;
+  }*/
+  do {
+   printf("\n%d", fibonacci_3);
+    fibonacci_1 = fibonacci_2;
+    fibonacci_2 = fibonacci_3;
+    fibonacci_3 = fibonacci_1 + fibonacci_2;
+    i++;   
+  } while (i <= number_of_terms);
 
   return 0;
 }
 
-/* Output
 
-/tmp/uJK4kAQksK.o
-Enter the number of terms (greater than 2): 8
+/* /tmp/tkrqYpmq4k.o
+Enter the number of terms (greater than 2): 5
 Fibonacci Series: 
 0 
 1
 1
 2
 3
-5
-8
-13
 
-=== Code Execution Successful ===
-
-*/
+=== Code Execution Successful ===*/
