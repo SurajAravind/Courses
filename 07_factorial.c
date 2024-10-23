18b97ddb51b4e11cd5d2b72496d6ae85841a5c92
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2024-10-23 08:18:22 +0530

    Update 07_factorial.c
    Added recursive feature for finding factorial of a number

--- 07_factorial.c
+++ 07_factorial.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+int recursive_factorial(int n) {
+    if (n == 0)
+      return 1;
+    else
+      return n * recursive_factorial(n-1);
+}
 int main() {
   int number, factorial_number=1;
   printf("Enter a non-negative number: ");
@@ -11,16 +17,26 @@ int main() {
         factorial_number *= i;
     printf("Factorial of %d is %d", number, factorial_number);      
   }
+  
+  printf("\nUsing recursive method Factorial of %d is %d", number, recursive_factorial(number));
+  
   return 0;
 }
 
+
 /*OUTPUT
-Enter a non-negative number: -3
-Entered a negative number.  Factorial cannot be determined
+Enter a non-negative number: 7
+Factorial of 7 is 5040
+Using recursive method Factorial of 7 is 5040
 
 Enter a non-negative number: 0
 Factorial of 0 is 1
+Using recursive method Factorial of 0 is 1
 
-Enter a non-negative number: 5
-Factorial of 5 is 120
+Enter a non-negative number: -4
+Segmentation fault
+
+The code has to be modified to check for negative number
+
+=== Code Execution Successful ===
 */
