2bea5706aec7e44ea2f3c426386c9d2ee19eab25
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2025-12-16 13:46:35 +0530

    Implement arithmetic operations on two numbers
    Added arithmetic operations for two numbers including addition, subtraction, multiplication, and division with error handling for division by zero.

--- 24CSEN1041/first.py
+++ 24CSEN1041/first.py
@@ -7,3 +7,33 @@ sum_of_numbers = num1 + num2
 
 # Print the result
 print("The sum of", num1, "and", num2, "is:", sum_of_numbers)
+
+
+
+
+# Simple program to perform arithmetic operations on two numbers
+
+# accept input from user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# perform operations
+print("Addition      :", a + b)
+print("Subtraction   :", a - b)
+print("Multiplication:", a * b)
+
+# handle division carefully (avoid divide by zero)
+if b != 0:
+    print("Division      :", a / b)
+else:
+    print("Division      : Not possible (division by zero)")
+
+Output
+===================== RESTART: E:/Python-Programs/first.py =====================
+Enter first number: 5
+Enter second number: 6
+Addition      : 11.0
+Subtraction   : -1.0
+Multiplication: 30.0
+Division      : 0.8333333333333334
+
