fee520911ac6f3a01a448613391247c1c4c54393
Suraj Aravind B <63137467+SurajAravind@users.noreply.github.com>
2026-02-13 11:07:44 +0530

    Add tuple example with unpacking and properties

---
+++ 24CSEN1041/11_tuple_related.py
@@ -0,0 +1,17 @@
+tuple_1 = ("2025000733", "2025001175", "2025003115")
+print(tuple_1)
+print(type(tuple_1))
+print(len(tuple_1))
+# Tuples are immutable just like strings
+
+# unpacking of a tuple
+sno_1, sno_2, sno_3 = tuple_1
+print(sno_1, sno_2, sno_3)
+
+#OUTPUT
+('2025000733', '2025001175', '2025003115')
+<class 'tuple'>
+3
+2025000733 2025001175 2025003115
+
+=== Code Execution Successful ===
